38061230cd665c7bb2969f1e0017212a662784ac
Talgat The Random <36820511+RND332@users.noreply.github.com>
2024-06-10 06:20:13 +0300

    chore: Update astro.config.mjs with site and build configurations

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,10 @@ import mdx from '@astrojs/mdx';
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), mdx()],
+  site: 'https://rnd332.com',
+  build: {
+     inlineStylesheets: 'always',
+  },
   image: {
     service: passthroughImageService()
   }
