f21da6c25236c095c5a3ae28dff12236d04ea84e
Talgat The Random <36820511+RND332@users.noreply.github.com>
2024-06-15 01:33:26 +0300

    chore: Update astro.config.mjs with sitemap integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,14 @@ import { defineConfig, passthroughImageService } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import mdx from '@astrojs/mdx';
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), mdx()],
+  integrations: [tailwind(), mdx(), sitemap()],
   site: 'https://rnd332.com',
   build: {
-     inlineStylesheets: 'always',
+    inlineStylesheets: 'always'
   },
   image: {
     service: passthroughImageService()
