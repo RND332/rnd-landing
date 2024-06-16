db117ba4629cd4316872a0ef419757c16427631a
Talgat The Random <36820511+RND332@users.noreply.github.com>
2024-06-16 10:38:40 +0300

    chore: Update package.json and astro.config.mjs for new @playform/compress dependency

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import mdx from '@astrojs/mdx';
-import { shield } from '@kindspells/astro-shield'
+import { shield } from '@kindspells/astro-shield';
 import sitemap from "@astrojs/sitemap";
+import playformCompress from "@playform/compress";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), mdx(), sitemap(), shield({})],
+  integrations: [tailwind(), mdx(), sitemap(), shield({}), playformCompress()],
   site: 'https://rnd332.com',
   build: {
     inlineStylesheets: 'always'
