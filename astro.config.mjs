2fd32b6889f9673b15bbb934e61e0446b2690d09
Talgat The Random <36820511+RND332@users.noreply.github.com>
2024-06-16 13:06:00 +0300

    chore: Update npm dependencies and import astro-compress for image compression

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,21 +4,15 @@ import mdx from '@astrojs/mdx';
 import { shield } from '@kindspells/astro-shield';
 import sitemap from "@astrojs/sitemap";
 import partytown from "@astrojs/partytown";
-import playformCompress from "@playform/compress";
+import compress from "astro-compress";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    tailwind(),
-    mdx(),
-    sitemap(),
-    shield({}),
-    playformCompress(),
-    partytown({
-      config: {
-        forward: ["dataLayer.push"],
-      },
-    })],
+  integrations: [tailwind(), mdx(), sitemap(), shield({}), partytown({
+    config: {
+      forward: ["dataLayer.push"]
+    }
+  }), compress()],
   site: 'https://rnd332.com',
   build: {
     inlineStylesheets: 'always'
