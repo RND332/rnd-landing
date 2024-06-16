bcf9218e17bb2818199a3bd7b4b870579752673a
Talgat The Random <36820511+RND332@users.noreply.github.com>
2024-06-16 11:19:43 +0300

    chore: Update npm dependencies and add @astrojs/partytown for Google Analytics tracking

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,9 +5,21 @@ import { shield } from '@kindspells/astro-shield';
 import sitemap from "@astrojs/sitemap";
 import playformCompress from "@playform/compress";
 
+import partytown from "@astrojs/partytown";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), mdx(), sitemap(), shield({}), playformCompress()],
+  integrations: [
+    tailwind(),
+    mdx(),
+    sitemap(),
+    shield({}),
+    playformCompress(),
+    partytown({
+      config: {
+        forward: ["dataLayer.push"],
+      },
+    })],
   site: 'https://rnd332.com',
   build: {
     inlineStylesheets: 'always'
