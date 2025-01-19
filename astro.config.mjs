95a9d5d9ded7b895dd815dc35ebd619b84c12842
RND332 <eureq631@gmail.com>
2025-01-19 09:35:14 +0300

    Add new effect on socials page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,27 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-import mdx from '@astrojs/mdx';
-import { shield } from '@kindspells/astro-shield';
+import mdx from "@astrojs/mdx";
+import { shield } from "@kindspells/astro-shield";
 import sitemap from "@astrojs/sitemap";
 import partytown from "@astrojs/partytown";
 import compress from "astro-compress";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), mdx(), sitemap(), shield({}), partytown({
-    config: {
-      forward: ["dataLayer.push"]
-    }
-  }), compress()],
-  site: 'https://rnd332.com',
-  build: {
-    inlineStylesheets: 'always'
-  }
+	integrations: [
+		tailwind(),
+		mdx(),
+		sitemap(),
+		shield({}),
+		partytown({
+			config: {
+				forward: ["dataLayer.push"],
+			},
+		}),
+		compress(),
+	],
+	site: "https://rnd332.com",
+	// build: {
+	//   inlineStylesheets: 'always'
+	// }
 });
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
 	theme: {
-		extend: {},
+		extend: {
+			keyframes: {
+				"gradient-shift": {
+					"0%": { backgroundPosition: "0% 50%" },
+					"50%": { backgroundPosition: "100% 50%" },
+					"100%": { backgroundPosition: "0% 50%" },
+				},
+				"socials-shift": {
+					"100%": { transform: "translateY(-384px)" },
+				},
+			},
+			animation: {
+				gradient: "gradient-shift 4s ease infinite",
+				socials: "socials-shift 10s linear infinite",
+			},
+		},
 	},
-	plugins: [],
-}
+	plugins: [require("tailwindcss-animate")],
+};
